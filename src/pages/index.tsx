f96a917a5f8d99727ac99aa7f9f82813a6e8632e
Thomás Escolástico Bueno <62290004+thomasbueno@users.noreply.github.com>
2022-03-27 17:28:07 -0300

    changing to SSG( Static Site Generation )

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1Ki1xPLrs6FgJNex0BN7EGYK");
 
   const product = {
@@ -52,5 +52,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
